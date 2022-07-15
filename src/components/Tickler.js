f338dec9ac68eee826c367992df9c31abd3116fa
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-07-16 01:37:09 +0300

    commit tree bfc15d194de2b74ec44836fdc57e46f33f2b02d6

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -5,7 +5,13 @@ function Tickler() {
     console.log("Teehee!");
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
+  
+/*
+function Tickler() {
+  return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>
+}
+*/
 
 export default Tickler;
